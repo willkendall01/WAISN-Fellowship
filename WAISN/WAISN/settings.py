e02ed16a92723642da7099eb1db19f0311946038
willkendall01 <wpk235@gmail.com>
2021-09-16 13:00:53 -0400

    Formatting

--- WAISN/WAISN/settings.py
+++ WAISN/WAISN/settings.py
@@ -28,7 +28,7 @@ EMAIL_USE_SSL = True
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
+#
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-*#n0u+xu(4tyi@*3sy%j3dl%o$l%bc#q0j+!+_gr7cwt@3*0nw'
 

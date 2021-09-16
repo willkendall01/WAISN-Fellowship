78cd5bccc8317124188999d4111a8e813d7d6808
willkendall01 <wpk235@gmail.com>
2021-09-16 12:42:22 -0400

    Hosting Params in Settings

--- WAISN/WAISN/settings.py
+++ WAISN/WAISN/settings.py
@@ -131,8 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = 'WAISN/Database/Static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
